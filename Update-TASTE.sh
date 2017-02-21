4a3ea0180314aed5cad5dced775c2424a24603ee
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-02-21 17:02:26 +0100

    Auto-merge all the changes done in upstream submodules.

--- Update-TASTE.sh
+++ Update-TASTE.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+git pull || exit 1
+git submodule init || exit 1
+git submodule update || exit 1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for INSTALL_SCRIPT in install/* ; do
     ${INSTALL_SCRIPT} || { echo Failed in execution of "${INSTALL_SCRIPT}" ; exit 1 ; }
