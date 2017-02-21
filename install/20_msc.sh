924e39bc02449429aa6f251c12ca69705fa4d43a
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-02-21 17:04:21 +0100

    Fix more paths

--- install/20_msc.sh
+++ install/20_msc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TASTE_PATHS=$HOME/.bashrc.taste
 
 # Build MSC tools
 cd $DIR/../taste-MSC/ || exit 1
@@ -7,5 +8,6 @@ make || exit 1
 cp ./MSC-Antlr3/bin/msc2py.exe $HOME/.local/bin || exit 1
 cp ./MSC-Antlr3/bin/taste-extract-asn-from-design.exe $HOME/.local/bin || exit 1
 
+# Install them in .local/bin
 PATH_CMD='export PATH=$PATH:$HOME/.local/bin'
 grep "${PATH_CMD}" ${TASTE_PATHS} >/dev/null || echo "${PATH_CMD}" >> ${TASTE_PATHS}
--- install/50_opengeode.sh
+++ install/50_opengeode.sh
@@ -2,3 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../opengeode || exit 1
 pip install --user --upgrade . || exit 1
+
+# Add .local/bin to PATH
+PATH_CMD='export PATH=$PATH:$HOME/.local/bin'
+grep "${PATH_CMD}" ${TASTE_PATHS} >/dev/null || echo "${PATH_CMD}" >> ${TASTE_PATHS}
