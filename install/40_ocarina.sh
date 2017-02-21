36d7fa6372508c2b694184037aeabe7d4a3f316d
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-02-21 17:49:11 +0100

    Tremendous optimization - skip Ocarina build if Git tree is clean and already installed.

--- install/40_ocarina.sh
+++ install/40_ocarina.sh
@@ -4,6 +4,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Setup the tools in ~/.local/bin
 cd $DIR/../ocarina || exit 1
+
+# Skip Ocarina building if tree is clean and version is identical
+HEAD="$(git log --oneline | head -1 | cut -d' ' -f1)"
+VERSION_INSTALLED="$(ocarina -v 2>&1 | grep ^Oca | awk '{print $NF}' | sed 's,),,;s,r,,')"
+git status >/dev/null
+TREE_CLEAN=$?
+if [ ${TREE_CLEAN} -eq 0 -a "${HEAD}" == "${VERSION_INSTALLED}" ] ; then
+    echo Ocarina tree is clean and already installed. Skipping Ocarina build...
+    exit 0
+fi
+
 make distclean # ignore any errors here
 ./support/reconfig || exit 1
 ./configure --prefix=${PREFIX} || exit 1
