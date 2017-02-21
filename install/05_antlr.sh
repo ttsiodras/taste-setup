e0727122e771792b90cbbbbe55ee979c043dd201
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-02-21 17:52:40 +0100

    Skip Python antlr install if already installed.

--- install/05_antlr.sh
+++ install/05_antlr.sh
@@ -4,7 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Setup ANTL3 support in ~/.local/bin
 cd $DIR/../antlr/antlr_python_runtime-3.1.3 || exit 1
-pip2 install --user --upgrade . || exit 1
+pip2 freeze | grep antlr-python-runtime >/dev/null \
+    || pip2 install --user --upgrade . || exit 1
 
 # Add .local/bin to PATH
 PATH_CMD='export PATH=$PATH:$HOME/.local/bin'
