e36f50f58449e7bf0dd33cf298a660e36ef865dd
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-02-21 16:03:20 +0100

    Each install script is responsible for adding the tools to the PATH

--- install/10_dmt.sh
+++ install/10_dmt.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TASTE_PATHS=$HOME/.bashrc.taste
+
+# Setup the tools in ~/.local/bin
 cd $DIR/../dmt || exit 1
-pip install --user --upgrade . || exit 1
+pip3 install --user --upgrade . || exit 1
+
+# Add .local/bin to PATH
+PATH_CMD='export PATH=$PATH:$HOME/.local/bin'
+grep "${PATH_CMD}" ${TASTE_PATHS} || echo "${PATH_CMD}" >> ${TASTE_PATHS}
--- install/15_asn1scc.sh
+++ install/15_asn1scc.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+# Fetch and install latest ASN1SCC release
 mkdir -p $HOME/tool-inst/share/asn1scc/ || exit 1
 cd $HOME/tool-inst/share/  || exit 1
 wget -q -O - https://github.com/ttsiodras/asn1scc/releases/download/3.3.9/asn1scc-bin-3.3.9.tar.gz \
     | tar zxvf -
+
+# Add ~/tool-inst/share/asn1scc/ to PATH
+PATH_CMD='export PATH=$PATH:$HOME/tool-inst/share/asn1scc/'
+grep "${PATH_CMD}" ${TASTE_PATHS} || echo "${PATH_CMD}" >> ${TASTE_PATHS}
