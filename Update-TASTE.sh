bb9bd8696757ce8a9da63da088e56bd0e8fbc161
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-02-21 17:32:31 +0100

    Only execute the init.d-like scripts

--- Update-TASTE.sh
+++ Update-TASTE.sh
@@ -3,6 +3,6 @@ git pull || exit 1
 git submodule init || exit 1
 git submodule update || exit 1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-for INSTALL_SCRIPT in install/* ; do
+for INSTALL_SCRIPT in install/[0-9]*sh ; do
     ${INSTALL_SCRIPT} || { echo Failed in execution of "${INSTALL_SCRIPT}" ; exit 1 ; }
 done
