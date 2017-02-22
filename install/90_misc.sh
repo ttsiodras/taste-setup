3995aec18afa42221563bfc46c435b977212393c
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-02-22 11:28:33 +0100

    Fixing installation of misc items

--- install/90_misc.sh
+++ install/90_misc.sh
@@ -2,6 +2,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . ${DIR}/common.sh
 
+cd ${DIR}/../misc || exit 1
+
 # taste-config
 mkdir -p ${PREFIX}/bin
 sed -e "s:INSTALL_PREFIX:${PREFIX}:g" taste-config.pl > taste-config.pl.tmp
