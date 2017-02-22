40f8ebb5737d3cb859d01b67b5804e252dfaa255
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-02-22 12:56:51 +0100

    Install the MSC utils alongside ASN1SCC (they share DLLs)

--- install/20_msc.sh
+++ install/20_msc.sh
@@ -6,9 +6,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../taste-MSC/ || exit 1
 make || exit 1
 
-# Install them in .local/bin
-cp ./MSC-Antlr3/bin/msc2py.exe $HOME/.local/bin || exit 1
-cp ./MSC-Antlr3/bin/taste-extract-asn-from-design.exe $HOME/.local/bin || exit 1
+# Install them alongside ASN1SCC (they share DLLs)
+cp ./MSC-Antlr3/bin/msc2py.exe ${PREFIX}/share/asn1scc/ || exit 1
+cp ./MSC-Antlr3/bin/taste-extract-asn-from-design.exe ${PREFIX}/share/asn1scc/ || exit 1
 
 # Update the path
 PATH_CMD='export PATH=$PATH:$HOME/.local/bin'
