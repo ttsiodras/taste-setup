91e4b8c7c00f59e01d057b6b35cb82be96e31287
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-02-22 10:19:25 +0100

    Install ASN1SCC only if it's not there yet.

--- install/15_asn1scc.sh
+++ install/15_asn1scc.sh
@@ -5,8 +5,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Fetch and install latest ASN1SCC release
 mkdir -p ${PREFIX}/share/asn1scc/ || exit 1
 cd ${PREFIX}/share/  || exit 1
-wget -q -O - https://github.com/ttsiodras/asn1scc/releases/download/3.3.9/asn1scc-bin-3.3.9.tar.gz \
-    | tar zxvf -
+VER=$(asn1.exe 2>&1 | grep '^Current' | awk '{print $NF}')
+if [ "${VER}" != "3.3.09" ] ; then
+    wget -q -O - https://github.com/ttsiodras/asn1scc/releases/download/3.3.9/asn1scc-bin-3.3.9.tar.gz \
+        | tar zxvf -
+fi
 
 # Add to PATH
 PATH_CMD='export PATH=$PATH:'"${PREFIX}/share/asn1scc/"
