c9c58b3b4b941b0417a52343b6e3a9a24040d770
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-02-23 10:30:03 +0100

    Many fixups and omissions addressed after a chroot session with Maxime.

--- install/30_qemu.sh
+++ install/30_qemu.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Setup the tools in ${PREFIX}/bin
 cd $DIR/../qemu-Leon3 || exit 1
-cp -a qemu-Leon3/qemu-Leon3*.sh ${PREFIX}/bin/
+cp -a qemu-Leon3*.sh ${PREFIX}/bin/ || exit 1
 
 # Add Leon simulators to PATH
 PATH_CMD='export PATH=$PATH:'"${PREFIX}/bin"
--- install/90_misc.sh
+++ install/90_misc.sh
@@ -19,19 +19,24 @@ mkdir -p ${PREFIX}/share/taste-types
 cp taste-common-types/taste-types.asn ${PREFIX}/share/taste-types/ || exit 1
 
 # Gnuplot
-cp gnuplot/driveGnuPlotsStreams.pl ${PREFIX}/bin/taste-gnuplot-streams
+cp gnuplot/driveGnuPlotsStreams.pl ${PREFIX}/bin/taste-gnuplot-streams || exit 1
 
 # PeekPoke component
 mkdir -p ${PREFIX}/share/peekpoke
 mkdir -p ${PREFIX}/share/peekpoke/component
-cp peek-poke/peekpoke.py ${PREFIX}/share/peekpoke/peekpoke.py
-cp peek-poke/PeekPoke.glade ${PREFIX}/share/peekpoke/PeekPoke.glade
+cp peek-poke/peekpoke.py ${PREFIX}/share/peekpoke/peekpoke.py || exit 1
+cp peek-poke/PeekPoke.glade ${PREFIX}/share/peekpoke/PeekPoke.glade || exit 1
 for i in DataView.aadl DataView.asn export_PeekPoke.aadl taste_probe.zip ; do \
-        cp peek-poke/component/$i ${PREFIX}/share/peekpoke/component/$i ; \
+        cp peek-poke/component/$i ${PREFIX}/share/peekpoke/component/$i || exit 1; \
 done
 
 # Helper scripts
 cp -a helper-scripts/* ${PREFIX}/bin/
+
+# Our own version (potentially with patches) of the AADL configuration files
+mkdir -p ${PREFIX}/share/config_ellidiss || exit 1
+cp ellidiss/TASTE_IV_Properties.aadl ${PREFIX}/share/config_ellidiss || exit 1
+cp ellidiss/TASTE_DV_Properties.aadl ${PREFIX}/share/config_ellidiss || exit 1
 
 # Ellidiss - point the PATH to the repo
 REAL_ELLIDISS=$(realpath "${DIR}/../ellidiss-GUI/TASTE-linux/bin")
