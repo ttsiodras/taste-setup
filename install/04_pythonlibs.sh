7ab45db24c99e464bd94ba52637d553013aab8e5
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-04-26 10:39:36 +0200

    In many distros, pip now points to pip3. Be specific (pip2)

--- install/04_pythonlibs.sh
+++ install/04_pythonlibs.sh
@@ -2,9 +2,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . ${DIR}/common.sh
 
-pip freeze | grep singledispatch >/dev/null \
-    || pip install --user --upgrade singledispatch || exit 1
-pip freeze | grep stringtemplate3 >/dev/null \
-    || pip install --user --upgrade stringtemplate3 || exit 1
-pip freeze | grep enum34 >/dev/null \
-    || pip install --user --upgrade enum34 || exit 1
+pip2 freeze | grep singledispatch >/dev/null \
+    || pip2 install --user --upgrade singledispatch || exit 1
+pip2 freeze | grep stringtemplate3 >/dev/null \
+    || pip2 install --user --upgrade stringtemplate3 || exit 1
+pip2 freeze | grep enum34 >/dev/null \
+    || pip2 install --user --upgrade enum34 || exit 1
--- install/54_speedometer.sh
+++ install/54_speedometer.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . ${DIR}/common.sh
 
 cd $DIR/../speedometer || exit 1
-pip install --user --upgrade . || exit 1
+pip2 install --user --upgrade . || exit 1
 
 # Add .local/bin to PATH
 PATH_CMD='export PATH=$PATH:$HOME/.local/bin'
