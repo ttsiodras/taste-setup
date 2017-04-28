c22a2aedfcf6cd7923d1c6aa0b4e66051c0004b4
Maxime Perrotin <Maxime.Perrotin@esa.int>
2017-04-28 15:46:58 +0200

    add install script for MSC editor

---
+++ install/56_msc-editor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. ${DIR}/common.sh
+
+cd $DIR/../msc-editor || exit 1
+pip2 install --user --upgrade . || exit 1
+
+# Add .local/bin to PATH
+PATH_CMD='export PATH=$PATH:$HOME/.local/bin'
+UpdatePATH
