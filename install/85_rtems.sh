9264cde688774436ec05d004250636d5bcf4eb0e
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-05-01 15:45:28 +0200

    Error handling in update of RTEMS compiler

--- install/85_rtems.sh
+++ install/85_rtems.sh
@@ -34,6 +34,9 @@ if wget -q -O /tmp/syncup.$$ "$URL_OF_NEW_RTEMS_INFO" ; then
             fi
             echo $NEW_RTEMS_MTIME $NEW_RTEMS_FOLDER | \
                 sudo tee "$INSTALLED_RTEMS_INFO"
+        else
+            echo Failed to download the new compiler... Aborting.
+            exit 1
         fi
     fi
 else
