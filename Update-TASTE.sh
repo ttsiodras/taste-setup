8de15b6a1939ff6d3b702e84724415f16b6be9cd
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-05-03 15:33:08 +0200

    Don't show the TASTE banner message during invocation of update scripts

--- Update-TASTE.sh
+++ Update-TASTE.sh
@@ -3,6 +3,7 @@ git pull || exit 1
 git submodule init || exit 1
 git submodule update || exit 1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export DISABLE_TASTE_BANNER=1
 for INSTALL_SCRIPT in install/[0-9]*sh ; do
     ${INSTALL_SCRIPT} || { echo Failed in execution of "${INSTALL_SCRIPT}" ; exit 1 ; }
 done
