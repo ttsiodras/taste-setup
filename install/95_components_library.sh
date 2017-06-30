5c3614aacb70c9a949c4ef7bf4b5406ea7ec8cc3
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-06-30 10:35:58 +0200

    Installing and updating component library.

---
+++ install/95_components_library.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. ${DIR}/common.sh
+
+# Fetch and install latest ASN1SCC release
+LIBDIR=${PREFIX}/share/components_library
+mkdir -p ${LIBDIR} || exit 1
+
+getver() {
+    grep TASTE_IV_Properties::Version "$@" | head -1 | \
+        awk -F\" '{print $(NF-1)}'
+}
+
+cd "${DIR}"/../components_library || exit 1
+for i in * ; do
+    # Only check folders (the components folder has files, too)
+    cd "${DIR}"/../components_library || exit 1
+    [ ! -d "$i" ] && continue
+    # Is it already installed?
+    if [ ! -d ${LIBDIR}/"$i" ] ; then
+        # No, install component in library
+        echo "[-] Installing $i in component library..."
+        cp -a "$i" ${LIBDIR} || exit 1
+    else
+        # Yes, it is - check for updates
+        cd "${DIR}"/../components_library/"$i" || exit 1
+        NEWVER=$(getver *aadl)
+        cd ${LIBDIR}/"$i" || exit 1
+        OLDVER=$(getver *aadl)
+        if [ "${OLDVER}" != "${NEWVER}" ] ; then
+            echo "[-] Updating $i in component library..."
+            cp -a "${DIR}"/../components_library/"$i"/* . 
+        fi
+    fi
+done
