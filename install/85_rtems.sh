9182b0306cca35a0c32d3c081cdd6f4104c81d77
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-07-15 19:51:40 +0200

    Add symlink from original path - RTEMS toolchain "burns" hardcoded paths...

--- install/85_rtems.sh
+++ install/85_rtems.sh
@@ -60,3 +60,9 @@ cat $HOME/.bashrc.taste | \
     sed 's,opt/rtems-4.[0-9]*/sparc-rtems4.[0-9]*/leon.,opt/rtems-4.12/sparc-rtems4.12/leon3,' \
     > ${TMPCFG}
 mv ${TMPCFG} $HOME/.bashrc.taste
+
+# Add symlink from original path - RTEMS toolchain "burns" hardcoded paths!
+[ -f /opt/rtems-4.12/bin/sparc-rtems4.12-gcc ] && \
+    /opt/rtems-4.12/bin/sparc-rtems4.12-gcc -v 2>&1 | \
+    grep 4f3b8da031e42d126afd94d17582123f66b78a68 && \
+    { cd /opt ; sudo ln -s rtems-4.12 rtems-4.11.2-SMP-FPU-2017.07.13 ; }
