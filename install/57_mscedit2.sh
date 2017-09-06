e88bb142990a56668e1d2b78c887c5398f681fc4
Ivan Sinkarenko <ivan.sinkarenko@esa.int>
2017-09-06 19:01:37 +0200

    Updated mscedit2 setup. Added missing dev tools

--- install/57_mscedit2.sh
+++ install/57_mscedit2.sh
@@ -2,7 +2,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . ${DIR}/common.sh
 
+# Install development tools to compile (pyside-rcc)
+# Note: Opengeode does not need this, because they check in
+# all temporary files into the repo. These have been ignored in
+# mscedit2, therefore they need to be compiled explicitly.
+sudo apt install -y pyside-tools
+
 cd $DIR/../mscedit2 || exit 1
+make
 pip2 install --user --upgrade . || exit 1
 
 # Add .local/bin to PATH
