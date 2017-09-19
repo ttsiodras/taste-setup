a2120328a788610ca44f6a6b4e2149ac8e6afd3a
Maxime Perrotin <Maxime.Perrotin@esa.int>
2017-09-19 16:31:53 +0200

    Add basis for a generic progress bar

---
+++ misc/helper-scripts/taste-progress-dialog.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import sys
+import PySide
+import time
+import signal
+
+from PySide.QtCore import QThread, Signal, QObject
+
+from PySide.QtGui import (QApplication,
+                          QMessageBox,
+                          QProgressDialog)
+
+class MyThread(QThread, QObject):
+    signal = Signal(str)
+    progress = Signal(int)
+    quit = Signal()
+    def run(self):
+        value = 10
+        while True:
+            # read from stdin without any buffering
+            line = sys.stdin.readline()
+            if len(line) == 0:
+                print ("Bye")
+                self.quit.emit()
+                return
+            elif line[0] == 'q':
+                print ("Quit")
+                self.quit.emit()
+                return
+            else:
+                self.signal.emit(line)
+                self.progress.emit(value)
+                value += 10
+                if value == 100:
+                    value = 0
+                time.sleep(0.1)
+
+
+def check_crazyflie():
+    ''' This function should be in a different module '''
+    return
+
+def check_snoopy():
+    raise NotImplementedError()
+
+
+def run_gui():
+        app = QApplication(sys.argv)
+        thread = MyThread()       
+        progress = QProgressDialog()
+        progress.setValue(0)
+        thread.signal.connect(progress.setLabelText)
+        thread.quit.connect(progress.cancel)
+        thread.progress.connect(progress.setValue)
+        thread.start()
+        progress.exec_()
+
+def main():
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    run_gui()
+
+if __name__ == '__main__':
+    main()
+
