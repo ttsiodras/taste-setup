9f1a0a869d6d33e48710fff068ae33213bf6940c
Thanassis Tsiodras <Thanassis.Tsiodras@esa.int>
2017-10-03 10:04:23 +0200

    DRY principle in overwriting the AADL components.

--- install/40_ocarina.sh
+++ install/40_ocarina.sh
@@ -2,13 +2,17 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "${DIR}/common.sh"
 
+function overWriteAADLcomponents()
+{
+    # Install Maxime's customized version of available deployment targets
+    echo Installing customized version of available deployment targets...
+    cat ../misc/supported-platforms/ocarina_components.aadl  | \
+        sed "s,/home/taste/tool-inst,$(taste-config --prefix)," > \
+        "${PREFIX}/share/ocarina/AADLv2/ocarina_components.aadl"
+}
+
 # Setup the tools in ~/.local/bin
 cd "$DIR/../ocarina" || exit 1
-
-# Install Maxime's customized version of available deployment targets
-cat ../misc/supported-platforms/ocarina_components.aadl  | \
-    sed "s,/home/taste/tool-inst,$(taste-config --prefix)," > \
-    ${PREFIX}/share/ocarina/AADLv2/ocarina_components.aadl
 
 # Skip Ocarina building if tree is clean and version is identical
 HEAD="$(git log --oneline | head -1 | cut -d' ' -f1)"
@@ -21,6 +25,7 @@ else
 fi
 if [ ${TREE_DIRTY} -eq 0 ] && [ "${HEAD}" == "${VERSION_INSTALLED}" ] ; then
     echo Ocarina tree is clean and already installed. Skipping Ocarina build...
+    overWriteAADLcomponents
     exit 0
 fi
 
@@ -29,6 +34,8 @@ make distclean # ignore any errors here
 ./configure --prefix="${PREFIX}" || exit 1
 make || exit 1
 make install
+
+overWriteAADLcomponents
 
 # Add Ocarina to PATH
 PATH_CMD='export PATH=$PATH:'"${PREFIX}/bin"
