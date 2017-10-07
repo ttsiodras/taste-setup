bde761a015e880d3d3d8da261a57b746463292b9
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-10-07 10:44:18 +0200

    A launch script to make life easier for Docker users.

---
+++ Docker-run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+if ! command -v docker ; then
+    echo "[x] You don't have Docker installed. Aborting..."
+    exit 1
+fi
+docker images | grep ^taste > /dev/null || {
+    echo "[x] You haven't built the TASTE Docker container."
+    echo '[x] You need to execute this to create it:'
+    echo '[x]'
+    echo '[x]     docker build -t taste .'
+    echo '[x]'
+    exit 1
+}
+echo '[-] ==========================================='
+echo '[-] Both Docker and the TASTE image were found.'
+echo '[-] You will be able to run all TASTE applications, including X11 ones,'
+echo -e "[-] and \e[1m\e[31m your \$HOME folder is mapped under /root/work (save files ONLY\e[0m"
+echo -e "[-]\e[1m\e[31m there, everything else will be deleted when the container dies).\e[0m"
+echo -e '[-] Also, \e[1m\e[32m the TASTE repositories are under /root/tool-src.\e[0m'
+echo '[-]'
+echo '[-] Launching the TASTE Docker container...'
+echo '[-]'
+HOMEB=/root
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch ${XAUTH}
+xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f ${XAUTH} nmerge -
+docker run \
+    -e DISPLAY \
+    -e XAUTHORITY=${XAUTH} \
+    -v ${XSOCK}:${XSOCK} \
+    -v ${XAUTH}:${XAUTH} \
+    -v "$HOME":"$HOMEB"/work \
+    -it taste /bin/bash -i
