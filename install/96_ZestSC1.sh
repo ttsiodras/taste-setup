aa4b5e3ad65d996f0b5b7c54b1f260e85ee078dc
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-10-15 16:56:40 +0200

    Install ZestSC1 FPGA library.

--- install/96_ZestSC1.sh
+++ install/96_ZestSC1.sh
@@ -6,3 +6,5 @@ sudo apt-get install -y --force-yes libusb-dev || exit 1
 
 cd ${DIR} || exit 1
 make -C ../misc/ZestSC1/linux/ || exit 1
+mkdir -p ${PREFIX}/share/ZestSC1
+cp -u ../misc/ZestSC1/linux/Lib/libZestSC1.a ${PREFIX}/share/ZestSC1/
