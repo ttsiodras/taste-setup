f129f61ea155103d5870efe4b0d77c7e81496de2
Maxime Perrotin <Maxime.Perrotin@esa.int>
2018-03-12 13:24:24 +0100

    Replace /home/assert with $HOME

--- qemu-Leon3/qemu-Leon3-gdb.sh
+++ qemu-Leon3/qemu-Leon3-gdb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Use sparc-rtems4.11-gdb to connect via 'tar extended-remote :9976'"
-/home/assert/tool-src/misc/qemu-Leon3/qemu-system-sparc -no-reboot -nographic -M leon3_generic -m 64M -kernel "$@" -gdb tcp::9976 -S
+$HOME/tool-src/misc/qemu-Leon3/qemu-system-sparc -no-reboot -nographic -M leon3_generic -m 64M -kernel "$@" -gdb tcp::9976 -S
