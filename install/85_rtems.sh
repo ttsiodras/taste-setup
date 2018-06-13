a83bb04ba8348e2b3f745e03942b66d7595c196c
Thanassis Tsiodras <Thanassis.Tsiodras@esa.int>
2018-06-13 16:58:25 +0200

    Updated RTEMS toolchain, now using SMP BSPs with driver manager enabled (for all 3: leon3, gr712rc, gr740).

--- install/85_rtems.sh
+++ install/85_rtems.sh
@@ -4,7 +4,7 @@ if [ "${TIMESTAMP}" != "Modify: 2018-06-05" ] ; then
     echo "Installing the latest RTEMS cross-compiler..."
     cd /opt || exit 1
     NEW_COMPILER_TARBALL=/tmp/newCompiler.$$.tar.gz
-    if wget -O $NEW_COMPILER_TARBALL "https://download.tuxfamily.org/taste/RTEMS/rtems-5.1-2018.03.08.tar.gz" ; then
+    if wget -O $NEW_COMPILER_TARBALL "https://download.tuxfamily.org/taste/RTEMS/rtems-5.1-2018.03.08.tar.bz2" ; then
         sudo tar zxvf $NEW_COMPILER_TARBALL || {
             echo Failed to extract $NEW_COMPILER_TARBALL...
             ls -l $NEW_COMPILER_TARBALL
