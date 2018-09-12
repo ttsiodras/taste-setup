b9ddc2ba4c5e5f45d0ec20f01a08637e8e6cc8c0
Thanassis Tsiodras <Thanassis.Tsiodras@esa.int>
2018-09-12 15:10:32 +0200

    Fix version handling of asn1-value-editor.

--- install/55_asn1valueEditor.sh
+++ install/55_asn1valueEditor.sh
@@ -2,13 +2,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "${DIR}/common.sh"
 
+VERSION_INSTALLED="$(taste-gui --version 2>&1 | head -1 | awk '{print $NF}')"
+
 # Setup asn1valueEditor in ~/.local/bin
 cd "$DIR/../asn1-value-editor" || exit 1
 
 # Skip install if the version installed is the same and the tree is clean
 HEAD="$(grep __version asn1_value_editor/asn1_value_editor.py  | head -1 | awk -F\" '{print $(NF-1);}')"
 
-VERSION_INSTALLED="$(taste-gui --version 2>&1 | head -1 | awk '{print $NF}')"
 GIT_OUTPUT=$(git status --porcelain)
 if [ "${GIT_OUTPUT}" == "" ] ; then
     TREE_DIRTY=0
