b1d3837c4fb336b598c2dfd64559ecd3ec7428f0
Thanassis Tsiodras <Thanassis.Tsiodras@esa.int>
2018-09-12 17:53:42 +0200

    MSC depends on an .stg file as well - install it.

--- install/20_msc.sh
+++ install/20_msc.sh
@@ -9,6 +9,7 @@ make || exit 1
 # Install them alongside ASN1SCC (they share DLLs)
 cp ./MSC-Antlr3/bin/msc2py.exe ${PREFIX}/share/asn1scc/ || exit 1
 cp ./MSC-Antlr3/bin/taste-extract-asn-from-design.exe ${PREFIX}/share/asn1scc/ || exit 1
+cp ./MSC-Antlr3/MSCtoPython.stg ${PREFIX}/share/asn1scc/ || exit 1
 
 # Install the MSC tracing-related tools (tracerd.py, etc) 
 cd $DIR/../misc/AutoGUI || exit 1
