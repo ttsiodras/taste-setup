3d20cebabb94a5f76e7fffbe30aff2a8ab785fb8
Thanassis Tsiodras <Thanassis.Tsiodras@esa.int>
2018-09-17 16:38:02 +0200

    Gaisler updated its toolchain.

---
+++ add-ons/install-gaisler-rcc-1.3-rc5.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. "${DIR}/common.sh"
+
+echo "[-] Checking if Gaisler's RCC1.3-rc5 is already under /opt/..."
+if [ -e /opt/rcc-1.3-rc5 ] ; then
+    echo '[-] /opt/rcc-1.3-rc5 is there already. Aborting...'
+    exit 1
+fi
+
+echo "[-] Downloading and uncompressing Gaisler's RCC1.3-rc5..."
+echo "[-]"
+cd /opt || exit 1
+wget -q -O - https://www.gaisler.com/anonftp/rcc/bin/linux/sparc-rtems-5-gcc-7.2.0-1.3-rc5-linux.txz | sudo tar Jxvf -
+if [ $? -ne 0 ] ; then
+    echo "Downloading Gaisler's RCC1.3-rc5 toolchain has failed."
+    echo Aborting...
+    exit 1
+fi
