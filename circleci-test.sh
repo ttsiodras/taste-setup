e336a555f97eff0c1d1cc4ef39d05accca6fbd1b
Thanassis Tsiodras <ttsiodras@gmail.com>
2018-10-21 09:30:21 +0200

    CircleCI 2.0: Properly handle artifacts folder.

--- circleci-test.sh
+++ circleci-test.sh
@@ -10,6 +10,6 @@ docker run \
     -e TASTE_IN_DOCKER \
     -e CIRCLECI \
     -e CI \
-    -v $(pwd)/artifacts:/tmp/artifacts \
+    -v "$(pwd)"/artifacts:/tmp/artifacts \
     -it taste \
-    /bin/bash -c 'apt-get install -y --force-yes xvfb ; Xvfb & export DISPLAY=:0 ; export GIT_SSL_NO_VERIFY=true ; cd /root/ ; . .bashrc.taste ; cd tool-src ; git fetch ; git checkout -f "${CIRCLE_BRANCH}" ; git branch --set-upstream-to=origin/${CIRCLE_BRANCH} ${CIRCLE_BRANCH} ; ./Update-TASTE.sh || exit 1 ; cd testSuites/ ; ./regression.py | tee /tmp/artifacts/A_complete_regression.log ; for i in Demo*/*.log ; do cp "$i" /tmp/artifacts/"${i/\//_}" ; done ; grep Demo /tmp/artifacts/A_complete_regression.log | grep -v OK$ && exit 1'
+    /bin/bash -c 'apt-get install -y --force-yes xvfb ; Xvfb & export DISPLAY=:0 ; export GIT_SSL_NO_VERIFY=true ; cd /root/ ; . .bashrc.taste ; cd tool-src ; git fetch ; git checkout -f "${CIRCLE_BRANCH}" ; git branch --set-upstream-to=origin/${CIRCLE_BRANCH} ${CIRCLE_BRANCH} ; ./Update-TASTE.sh || exit 1 ; cd testSuites/ ; ./regression.py | tee /tmp/artifacts/A_complete_regression.log ; for i in Demo*/*.log ; do cp "$i" /tmp/artifacts/"${i/\//_}" ; done ; grep Demo /tmp/artifacts/A_complete_regression.log | grep -v OK$ && exit 1; exit 0'
