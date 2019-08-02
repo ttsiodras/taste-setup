5d3ca87968953bda2d27b88b67907e90aeb42e8d
Thanassis Tsiodras <ttsiodras@gmail.com>
2019-08-02 10:50:05 +0200

    Added install script for AIR.

---
+++ install/86_air.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. ${DIR}/common.sh
+
+cd "$DIR"/../air || exit 1
+git submodule init
+git submodule update air/pos/rtems5
+
+cd air || exit 1
+
+# Pass the following configuration to AIR's "configure" script:
+#
+# Select the target architecture:
+# * [0]: sparc
+# Select the target board support package:
+# * [0]: leon3_or_tsim2
+#   [1]: tsim
+#   [2]: leon4
+# Select if FPU is:
+# * [0]: Enabled
+#   [1]: Disabled
+# Select debug monitor:
+# * [0]: GRMON
+#   [1]: DMON
+# Install All RTOS ?
+# * [0]: No
+#   [1]: Yes
+# Install posixrtems5?
+#   [0]: No
+# * [1]: Yes
+# Install rtems48i?
+# * [0]: No
+#   [1]: Yes
+# Install rtems5?
+#   [0]: No
+# * [1]: Yes
+# Install bare?
+# * [0]: No
+#   [1]: Yes
+echo -e "0\n0\n0\n0\n0\n1\n0\n1\n0\n\n" | ./configure
+make || exit 1
+
+# Add to PATH
+AIR_REAL_PATH="$(realpath $(pwd))"
+PATH_CMD='export PATH=$PATH:"'"${AIR_REAL_PATH}"'"'
+UpdatePATH
+
+PATH_CMD='export AIR="'"${AIR_REAL_PATH}"'"'
+UpdatePATH
