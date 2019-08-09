a09c9dad6baa90a83708026b6f10381bf3c9a000
Thanassis Tsiodras <ttsiodras@gmail.com>
2019-08-09 10:22:39 +0200

    Cleanup AdaCore mess in templates-parser.

--- install/87_kazoo.sh
+++ install/87_kazoo.sh
@@ -12,6 +12,13 @@ mkdir -p ${PREFIX}/share/kazoo || exit 1
 cp -a kazoo ${PREFIX}/share/kazoo || exit 1
 cp -a ../kazoo/templates ${PREFIX}/share/kazoo || exit 1
 
+# Sadly, AdaCore has not responded to our pull request:
+#    https://github.com/AdaCore/templates-parser/pull/21
+#
+# To avoid false notification of "dirty" git status,
+# clean up their mess in the "templates-parser" after their build
+rm "${DIR}/../kazoo/templates-parser/tp_xmlada.gpr"
+
 # Add kazoo binary to the PATH
 PATH_CMD='export PATH=$PATH:'"${PREFIX}/share/kazoo"
 UpdatePATH
