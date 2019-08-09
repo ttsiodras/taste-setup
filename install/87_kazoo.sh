5b8da598612aa0814d87c5949d7d674b4559eab0
Thanassis Tsiodras <Thanassis.Tsiodras@esa.int>
2019-08-09 11:40:17 +0200

    Appease shellcheck.

--- install/87_kazoo.sh
+++ install/87_kazoo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-. ${DIR}/common.sh
+. "${DIR}"/common.sh
 
 cd "$DIR"/../kazoo || exit 1
 git submodule init
@@ -8,9 +8,9 @@ git submodule update
 
 make || exit 1
 
-mkdir -p ${PREFIX}/share/kazoo || exit 1
-cp -a kazoo ${PREFIX}/share/kazoo || exit 1
-cp -a ../kazoo/templates ${PREFIX}/share/kazoo || exit 1
+mkdir -p "${PREFIX}"/share/kazoo || exit 1
+cp -a kazoo "${PREFIX}"/share/kazoo || exit 1
+cp -a ../kazoo/templates "${PREFIX}"/share/kazoo || exit 1
 
 # Sadly, AdaCore has not responded to our pull request:
 #    https://github.com/AdaCore/templates-parser/pull/21
@@ -18,7 +18,7 @@ cp -a ../kazoo/templates ${PREFIX}/share/kazoo || exit 1
 # To avoid false notification of "dirty" git status,
 # clean up their mess in the "templates-parser" after their build
 rm -f "${DIR}/../kazoo/templates-parser/tp_xmlada.gpr"
-rm -f "${DIR}/../kazoo/templates-parser/config/setup/foo.ads.std???"
+rm -f "${DIR}/../kazoo/templates-parser/config/setup/foo.ads.std"???
 
 
 # Add kazoo binary to the PATH
